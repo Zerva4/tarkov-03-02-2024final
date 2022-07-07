9ad2a94a9c3cf7eb6c0cb585f7405d3e858c3500
Alex Krasnov <alex@i-tools.ru>
2022-07-07 11:50:56 +0500

    Update email

--- migrations/Version20220707062027.php
+++ migrations/Version20220707062027.php
@@ -17,7 +17,7 @@ final class Version20220707062027 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $sql = $sql = <<<SQL
-            INSERT INTO user (email, roles, password) VALUES ('root', '[\"ROLE_ADMIN\"]', '\$2y\$13\$v03eQv4s2Ls2CwzH3hGOquxN.FDmpifIok8lhYyQTqAm2Xyz6ev3K')
+            INSERT INTO user (email, roles, password) VALUES ('root@root.tld', '[\"ROLE_ADMIN\"]', '\$2y\$13\$v03eQv4s2Ls2CwzH3hGOquxN.FDmpifIok8lhYyQTqAm2Xyz6ev3K')
         SQL;
         $this->addSql($sql);
     }
