5e404e78364b5e5764ac80cce49a0132ffe4de9d
Alex Krasnov <alex@i-tools.ru>
2022-07-11 10:24:04 +0500

    Changed db table name

--- src/Entity/Trader.php
+++ src/Entity/Trader.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\TraderRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Table(name: 'Traders')]
 #[ORM\Entity(repositoryClass: TraderRepository::class)]
 class Trader
 {
