0de6f2e73cae3538bdb1da73df14a8acae603b8a
Alex Krasnov <alex@i-tools.ru>
2022-08-04 18:04:41 +0500

    Added multi langs

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,43 +4,30 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use App\Interfaces\TagInterface;
 use App\Repository\ArticleRepository;
 use App\Traits\UuidPrimaryKeyTrait;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
 
 #[ORM\Table(name: 'articles')]
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
-class Article
+class Article implements TranslatableInterface
 {
     use UuidPrimaryKeyTrait;
     use TimestampableEntity;
+    use TranslatableTrait;
 
     #[ORM\Column(type: 'boolean')]
     private bool $published;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private string $title;
-
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private ?string $imagePoster;
 
-    #[ORM\Column(type: 'text', nullable: true)]
-    private ?string $description;
-
-    #[ORM\Column(type: 'text')]
-    private string $body;
-
-    #[ORM\JoinTable(name: 'articles_tags')]
-    #[ORM\ManyToMany(targetEntity: Tag::class, cascade: ['persist'])]
-    private ?Collection $tags;
-
-    public function __construct()
+    public function __construct(string $defaultLocation = '%app.default_locale%')
     {
-        $this->tags = new ArrayCollection();
+        $this->defaultLocale = $defaultLocation;
     }
 
     public function isPublished(): ?bool
@@ -55,18 +42,6 @@ class Article
         return $this;
     }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
     public function getImagePoster(): ?string
     {
         return $this->imagePoster;
@@ -77,72 +52,5 @@ class Article
         $this->imagePoster = $imagePoster;
 
         return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getBody(): ?string
-    {
-        return $this->body;
-    }
-
-    public function setBody(string $body): self
-    {
-        $this->body = $body;
-
-        return $this;
-    }
-
-    /**
-     * @return TagInterface[]|null
-     */
-    public function getTags(): ?Collection
-    {
-        return $this->tags;
-    }
-
-    /**
-     * @param ArrayCollection $tags
-     * @return Article
-     */
-    public function setTags(ArrayCollection $tags): self
-    {
-        $this->tags = $tags;
-
-        return $this;
-    }
-
-    /**
-     * @param TagInterface ...$tags
-     * @return Article
-     */
-    public function addTag(TagInterface ...$tags): self
-    {
-        foreach ($tags as $tag) {
-            if (!$this->tags->contains($tag)) {
-                $this->tags->add($tag);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param Tag $tag
-     * @return void
-     */
-    public function removeTag(TagInterface $tag): void
-    {
-        $this->tags->removeElement($tag);
     }
 }
