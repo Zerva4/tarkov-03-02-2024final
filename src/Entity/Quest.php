bea137023f327c69b9293c2205172c545443aeff
Alex Krasnov <alex@i-tools.ru>
2022-08-06 02:59:32 +0500

    Added mapping to Location and Trader entities

--- src/Entity/Quest.php
+++ src/Entity/Quest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Interfaces\LocationInterface;
+use App\Interfaces\QuestInterface;
+use App\Interfaces\TraderInterface;
 use App\Repository\QuestRepository;
 use App\Traits\TranslatableMagicMethodsTrait;
 use App\Traits\UuidPrimaryKeyTrait;
@@ -15,7 +18,7 @@ use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
 
 #[ORM\Table(name: 'Quests')]
 #[ORM\Entity(repositoryClass: QuestRepository::class)]
-class Quest implements TranslatableInterface, TimestampableInterface
+class Quest implements QuestInterface, TranslatableInterface, TimestampableInterface
 {
     use UuidPrimaryKeyTrait;
     use TimestampableTrait;
@@ -28,6 +31,12 @@ class Quest implements TranslatableInterface, TimestampableInterface
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private ?string $imageName;
 
+    #[ORM\ManyToOne(targetEntity: Trader::class, inversedBy: 'quests')]
+    private ?TraderInterface $trader = null;
+
+    #[ORM\ManyToOne(targetEntity: Location::class, inversedBy: 'quests')]
+    private ?LocationInterface $location = null;
+
     public function __construct(string $defaultLocation = '%app.default_locale%')
     {
         $this->defaultLocale = $defaultLocation;
@@ -38,7 +47,7 @@ class Quest implements TranslatableInterface, TimestampableInterface
         return $this->published;
     }
 
-    public function setPublished(bool $published): self
+    public function setPublished(bool $published): QuestInterface
     {
         $this->published = $published;
 
@@ -55,9 +64,50 @@ class Quest implements TranslatableInterface, TimestampableInterface
 
     /**
      * @param string|null $imageName
+     * @return QuestInterface
      */
-    public function setImageName(?string $imageName): void
+    public function setImageName(?string $imageName): QuestInterface
     {
         $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * @return TraderInterface|null
+     */
+    public function getTrader(): ?TraderInterface
+    {
+        return $this->trader;
+    }
+
+    /**
+     * @param TraderInterface|null $trader
+     * @return QuestInterface
+     */
+    public function setTrader(?TraderInterface $trader): QuestInterface
+    {
+        $this->trader = $trader;
+
+        return $this;
+    }
+
+    /**
+     * @return LocationInterface|null
+     */
+    public function getLocation(): ?LocationInterface
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param LocationInterface|null $location
+     * @return QuestInterface
+     */
+    public function setLocation(?LocationInterface $location): QuestInterface
+    {
+        $this->location = $location;
+
+        return $this;
     }
 }
