f5f9d3413ec66addb70395e1deae4ada1c0c581d
Alex Krasnov <alex@i-tools.ru>
2022-08-06 02:57:11 +0500

    Update methods

--- src/Interfaces/LocationInterface.php
+++ src/Interfaces/LocationInterface.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 namespace App\Interfaces;
 
+use Ramsey\Uuid\UuidInterface;
+
 interface LocationInterface
 {
+    /**
+     * @return UuidInterface
+     */
+    public function getId(): UuidInterface;
 
+    /**
+     * @param UuidInterface $id
+     * @return void
+     */
+    public function setId(UuidInterface $id): void;
 }
