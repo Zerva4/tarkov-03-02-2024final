de4621d790561f0af0eb0e43a4d4da30cdd9a743
Alex Krasnov <alex@i-tools.ru>
2022-08-08 12:43:37 +0500

    Append slug field and table index

--- src/Entity/Quest.php
+++ src/Entity/Quest.php
@@ -15,8 +15,10 @@ use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
 use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
 use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
 use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Table(name: 'Quests')]
+#[ORM\Index(columns: ['slug'], name: 'quests_slug_idx')]
 #[ORM\Entity(repositoryClass: QuestRepository::class)]
 class Quest implements QuestInterface, TranslatableInterface, TimestampableInterface
 {
@@ -36,6 +38,11 @@ class Quest implements QuestInterface, TranslatableInterface, TimestampableInter
 
     #[ORM\ManyToOne(targetEntity: Location::class, inversedBy: 'quests')]
     private ?LocationInterface $location = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
+    #[Assert\NotBlank]
+    #[Assert\Regex(pattern: '/^[a-z0-9]+(?:-[a-z0-9]+)*$/', message: 'Invalid format', match: true)]
+    private string $slug;
 
     public function __construct(string $defaultLocation = '%app.default_locale%')
     {
@@ -107,6 +114,25 @@ class Quest implements QuestInterface, TranslatableInterface, TimestampableInter
     public function setLocation(?LocationInterface $location): QuestInterface
     {
         $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     * @return Quest
+     */
+    public function setSlug(string $slug): QuestInterface
+    {
+        $this->slug = $slug;
 
         return $this;
     }
