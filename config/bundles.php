e4814da4e5b3c28923a50b5dcf27917d3e62aa1e
Alex Krasnov <alex@i-tools.ru>
2022-08-08 16:07:15 +0500

    Added liip bundle

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     A2lix\AutoFormBundle\A2lixAutoFormBundle::class => ['all' => true],
     A2lix\TranslationFormBundle\A2lixTranslationFormBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
