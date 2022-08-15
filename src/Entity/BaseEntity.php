eaa5366d4b8cb86f4b65fa92b266cb7fde564fb1
Alex Krasnov <alex@i-tools.ru>
2022-08-15 11:10:43 +0500

    Base entity

---
+++ src/Entity/BaseEntity.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Traits\TranslatableMagicMethodsTrait;
+use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+
+class BaseEntity implements TranslatableInterface, TimestampableInterface
+{
+    use TimestampableTrait;
+    use TranslatableTrait;
+    use TranslatableMagicMethodsTrait;
+
+    public function __construct(string $defaultLocation = '%app.default_locale%')
+    {
+        $this->defaultLocale = $defaultLocation;
+    }
+
+    protected function proxyCurrentLocaleTranslation(string $method, array $arguments = [])
+    {
+        if (! method_exists(self::getTranslationEntityClass(), $method)) {
+            $method = 'get' . ucfirst($method);
+        }
+
+        $translation = $this->translate($this->getCurrentLocale());
+
+        return (method_exists(self::getTranslationEntityClass(), $method)) ? call_user_func_array([$translation, $method], $arguments) : null;
+    }
+}
