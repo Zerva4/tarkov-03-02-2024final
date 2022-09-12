43ca17bc36e42985bf8a6ea7771d7b9c5df9d45d
Alex Krasnov <alex@i-tools.ru>
2022-09-12 10:05:31 +0500

    Added fileds minPlayersNumber, maxPlayersNumber and remove numberOfPlayers

--- src/Entity/Map.php
+++ src/Entity/Map.php
@@ -66,9 +66,11 @@ class Map implements MapInterface, TranslatableInterface, TimestampableInterface
      */
     private ?File $imageFile = null;
 
-    #[ORM\Column(type: 'string', length: 10, nullable: true)]
-    #[Assert\NotBlank]
-    private ?string $numberOfPlayers;
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private int $minPlayersNumber = 0;
+
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private int $maxPlayersNumber = 0;
 
     #[ORM\Column(type: 'time', nullable: true)]
     private ?DateTimeInterface $raidDuration;
@@ -80,12 +82,10 @@ class Map implements MapInterface, TranslatableInterface, TimestampableInterface
     private Collection $locations;
 
     // TODO: Добавить врагов и босссов.
-
     public function __construct(string $defaultLocation = '%app.default_locale%')
     {
         $this->defaultLocale = $defaultLocation;
         $this->quests = new ArrayCollection();
-
     }
 
     public function isPublished(): ?bool
@@ -93,119 +93,42 @@ class Map implements MapInterface, TranslatableInterface, TimestampableInterface
         return $this->published;
     }
 
-    public function setPublished(bool $published): self
+    public function setPublished(bool $published): MapInterface
     {
         $this->published = $published;
 
         return $this;
     }
 
-    public function getNumberOfPlayers(): ?string
+    public function getApiId(): string
     {
-        return $this->numberOfPlayers;
+        return $this->apiId;
     }
 
-    public function setNumberOfPlayers(?string $numberOfPlayers): self
+    public function setApiId(string $apiId): MapInterface
     {
-        $this->numberOfPlayers = $numberOfPlayers;
+        $this->apiId = $apiId;
 
         return $this;
     }
 
-    public function getRaidDuration(): ?DateTimeInterface
-    {
-        return $this->raidDuration;
-    }
-
-    public function setRaidDuration(?DateTimeInterface $raidDuration): self
-    {
-        $this->raidDuration = $raidDuration;
-
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
     public function getImageName(): ?string
     {
         return $this->imageName;
     }
 
-    /**
-     * @param string|null $imageName
-     */
-    public function setImageName(?string $imageName): void
+    public function setImageName(?string $imageName): MapInterface
     {
         $this->imageName = $imageName;
-    }
-
-    /**
-     * @return Collection
-     */
-    public function getQuests(): Collection
-    {
-        return $this->quests;
-    }
-
-    /**
-     * @param Collection $quests
-     * @return MapInterface
-     */
-    public function setQuests(Collection $quests): MapInterface
-    {
-        $this->quests = $quests;
 
         return $this;
     }
 
-    /**
-     * @param QuestInterface ...$quests
-     * @return MapInterface
-     */
-    public function addQuest(QuestInterface ...$quests): MapInterface
-    {
-        foreach ($quests as $quest) {
-            if (!$this->quests->contains($quest)) {
-                $this->quests->add($quest);
-                $quest->setLocation($this);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param QuestInterface $quest
-     * @return MapInterface
-     */
-    public function removeQuest(QuestInterface $quest): MapInterface
-    {
-        if ($this->quests->contains($quest)) {
-            $this->quests->removeElement($quest);
-            $quest->setLocation(null);
-        }
-
-        return $this;
-    }
-
-    public function __toString(): string
-    {
-        return $this->__get('title');
-    }
-
-    /**
-     * @return mixed
-     */
     public function getImageFile(): mixed
     {
         return $this->imageFile;
     }
 
-    /**
-     * @param mixed $imageFile
-     * @return MapInterface
-     */
     public function setImageFile(mixed $imageFile): MapInterface
     {
         $this->imageFile = $imageFile;
@@ -217,42 +140,96 @@ class Map implements MapInterface, TranslatableInterface, TimestampableInterface
         return $this;
     }
 
-    /**
-     * @return Collection
-     */
-    public function getLocations(): Collection
+    public function getMinPlayersNumber(): int
     {
-        return $this->locations;
+        return $this->minPlayersNumber;
     }
 
-    /**
-     * @param Collection $locations
-     */
-    public function setLocations(Collection $locations): void
+    public function setMinPlayersNumber(int $minPlayersNumber): MapInterface
     {
-        $this->locations = $locations;
+        $this->minPlayersNumber = $minPlayersNumber;
+
+        return $this;
     }
 
-    /**
-     * @param MapLocationInterface ...$locations
-     * @return MapInterface
-     */
-    public function addLocation(MapLocationInterface ...$locations): MapInterface
+    public function getMaxPlayersNumber(): int
     {
-        foreach ($locations as $location) {
-            if (!$this->locations->contains($location)) {
-                $this->locations->add($location);
-                $location->setMap($this);
-            }
+        return $this->maxPlayersNumber;
+    }
+
+    public function setMaxPlayersNumber(int $maxPlayersNumber): MapInterface
+    {
+        $this->maxPlayersNumber = $maxPlayersNumber;
+
+        return $this;
+    }
+
+    public function getRaidDuration(): ?DateTimeInterface
+    {
+        return $this->raidDuration;
+    }
+
+    public function setRaidDuration(?DateTimeInterface $raidDuration): MapInterface
+    {
+        $this->raidDuration = $raidDuration;
+
+        return $this;
+    }
+
+    public function getQuests(): Collection
+    {
+        return $this->quests;
+    }
+
+    public function setQuests(Collection $quests): MapInterface
+    {
+        $this->quests = $quests;
+
+        return $this;
+    }
+
+    public function addQuest(QuestInterface $quest): MapInterface
+    {
+        if (!$this->quests->contains($quest)) {
+            $this->quests->add($quest);
+            $quest->setMap($this);
         }
 
         return $this;
     }
 
-    /**
-     * @param MapLocationInterface $location
-     * @return MapInterface
-     */
+    public function removeQuest(QuestInterface $quest): MapInterface
+    {
+        if ($this->quests->contains($quest)) {
+            $this->quests->removeElement($quest);
+            $quest->setMap(null);
+        }
+
+        return $this;
+    }
+
+    public function getLocations(): Collection
+    {
+        return $this->locations;
+    }
+
+    public function setLocations(Collection $locations): MapInterface
+    {
+        $this->locations = $locations;
+
+        return $this;
+    }
+
+    public function addLocation(MapLocationInterface $location): MapInterface
+    {
+        if (!$this->locations->contains($location)) {
+            $this->locations->add($location);
+            $location->setMap($this);
+        }
+
+        return $this;
+    }
+
     public function removeLocation(MapLocationInterface $location): MapInterface
     {
         if ($this->locations->contains($location)) {
@@ -261,22 +238,6 @@ class Map implements MapInterface, TranslatableInterface, TimestampableInterface
         }
 
         return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getApiId(): string
-    {
-        return $this->apiId;
-    }
-
-    /**
-     * @param string $apiId
-     */
-    public function setApiId(string $apiId): void
-    {
-        $this->apiId = $apiId;
     }
 
     protected function proxyCurrentLocaleTranslation(string $method, array $arguments = [])
@@ -288,5 +249,10 @@ class Map implements MapInterface, TranslatableInterface, TimestampableInterface
         $translation = $this->translate($this->getCurrentLocale());
 
         return (method_exists(self::getTranslationEntityClass(), $method)) ? call_user_func_array([$translation, $method], $arguments) : null;
+    }
+
+    public function __toString(): string
+    {
+        return $this->__get('title');
     }
 }
