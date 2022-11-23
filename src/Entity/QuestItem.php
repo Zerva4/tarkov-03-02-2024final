751a4dea7d8c36d1b607887590ee8032171d1a3f
Alex Krasnov <alex@i-tools.ru>
2022-11-24 03:16:42 +0500

    Quest item entity

---
+++ src/Entity/QuestItem.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Entity;
+
+use App\Interfaces\QuestItemInterface;
+use App\Interfaces\UuidPrimaryKeyInterface;
+use App\Repository\QuestItemRepository;
+use App\Traits\SlugTrait;
+use App\Traits\UuidPrimaryKeyTrait;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+#[ORM\Table(name: 'quests_items')]
+#[ORM\Index(columns: ['slug'], name: 'quests_items_slug_idx')]
+#[ORM\Index(columns: ['api_id'], name: 'quests_items_api_key_idx')]
+#[ORM\Entity(repositoryClass: QuestItemRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+#[Vich\Uploadable]
+/**
+ * @Vich\Uploadable
+ */
+class QuestItem extends TranslatableEntity implements UuidPrimaryKeyInterface, QuestItemInterface, TranslatableInterface
+{
+    use UuidPrimaryKeyTrait;
+    use SlugTrait;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
+    private string $apiId;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $published;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $width = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $height = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageName = null;
+
+    #[Vich\UploadableField(mapping: 'quests', fileNameProperty: 'imageName')]
+    #[Assert\Valid]
+    #[Assert\File(
+        maxSize: '2M',
+        mimeTypes: ['image/jpg', 'image/gif', 'image/jpeg', 'image/png']
+    )]
+    /**
+     * @Vich\UploadableField(mapping="quests", fileNameProperty="imageName")
+     * @Assert\Valid
+     * @Assert\File(
+     *     maxSize="2M",
+     *     mimeTypes={
+     *         "image/jpg", "image/gif", "image/jpeg", "image/png"
+     *     }
+     * )
+     */
+    private ?File $imageFile = null;
+
+    public function getApiId(): string
+    {
+        return $this->apiId;
+    }
+
+    public function setApiId(string $apiId): QuestItemInterface
+    {
+        $this->apiId = $apiId;
+
+        return $this;
+    }
+
+    public function isPublished(): bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): QuestItemInterface
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getWidth(): ?int
+    {
+        return $this->width;
+    }
+
+    public function setWidth(int $width): QuestItemInterface
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    public function getHeight(): ?int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): QuestItemInterface
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(string $imageName): QuestItemInterface
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(?File $imageFile): QuestItemInterface
+    {
+        $this->imageFile = $imageFile;
+
+        return $this;
+    }
+}
