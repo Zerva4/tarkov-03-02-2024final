f51644f4022e20aa92580e70a692464bbfa9d3e2
Alex Krasnov <alex@i-tools.ru>
2023-01-26 18:41:32 +0500

    Front controller for items

---
+++ src/Controller/ItemsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ItemRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ItemsController extends AbstractController
+{
+    #[Route('/items', name: 'app_items')]
+    public function index(): Response
+    {
+        return $this->render('items/index.html.twig', [
+            'controller_name' => 'ItemsController',
+        ]);
+    }
+
+    #[Route('/items/{slug}', name: 'app_items_by_slug')]
+    public function getBySlug(string $slug, ItemRepository $itemRepository): Response
+    {
+        $item = $itemRepository->getItemBySlug($slug);
+        dump($item);
+
+        return $this->render('items/index.html.twig', [
+            'controller_name' => 'ItemsController',
+        ]);
+    }
+}
