e12ede997e34e756f21b12431770c6b0022270a8
Alex Krasnov <alex@i-tools.ru>
2023-01-27 00:59:09 +0500

    Update after removing

--- src/Entity/Quests/Quest.php
+++ src/Entity/Quests/Quest.php
@@ -21,7 +21,6 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
