00d629cb53088c50e905f747b3927aebf8a4e03d
Alex Krasnov <alex@i-tools.ru>
2023-03-02 18:27:33 +0500

    Updated

--- src/Interfaces/Workshop/PlaceInterface.php
+++ src/Interfaces/Workshop/PlaceInterface.php
@@ -10,8 +10,8 @@ interface PlaceInterface
     public function setApiId(string $apiId): PlaceInterface;
     public function isPublished(): bool;
     public function setPublished(bool $published): PlaceInterface;
-    public function getOrder(): int;
-    public function setOrder(int $order): PlaceInterface;
+    public function getOrderPlace(): ?int;
+    public function setOrderPlace(?int $order): PlaceInterface;
     public function getLevels(): Collection;
     public function setLevels(Collection $levels): PlaceInterface;
 }
