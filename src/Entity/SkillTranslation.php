68dacfeb3b7919cdef4bb465e2b19a4105888cdb
Alex Krasnov <alex@i-tools.ru>
2023-03-02 16:50:06 +0500

    Translation for Skill entity

---
+++ src/Entity/SkillTranslation.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Interfaces\UuidPrimaryKeyInterface;
+use App\Repository\LocationTranslationRepository;
+use App\Traits\UuidPrimaryKeyTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+
+#[ORM\Table(name: 'skills_translation')]
+#[ORM\Index(columns: ['locale'], name: 'skills_locale_idx')]
+#[ORM\Entity(repositoryClass: LocationTranslationRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class SkillTranslation implements UuidPrimaryKeyInterface, TranslationInterface, TimestampableInterface
+{
+    use UuidPrimaryKeyTrait;
+    use TranslationTrait;
+    use TimestampableTrait;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $title;
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getTitle();
+    }
+}
