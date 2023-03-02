7ca2110f468eda7a2a0ce2de29d34594482fb3d7
Alex Krasnov <alex@i-tools.ru>
2023-03-02 18:24:32 +0500

    Remove unused methods

--- src/Repository/MapLocationRepository.php
+++ src/Repository/MapLocationRepository.php
@@ -38,29 +38,4 @@ class MapLocationRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return MapLocation[] Returns an array of MapLocation objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?MapLocation
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
