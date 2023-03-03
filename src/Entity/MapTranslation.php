92e1b15e5bbd76b268d49334c97cb939bd1f9327
Alex Krasnov <alex@i-tools.ru>
2023-03-03 15:14:54 +0500

    Change repository name

--- src/Entity/MapTranslation.php
+++ src/Entity/MapTranslation.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Interfaces\UuidPrimaryKeyInterface;
-use App\Repository\LocationTranslationRepository;
+use App\Repository\MapTranslationRepository;
 use App\Traits\UuidPrimaryKeyTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
@@ -15,7 +15,7 @@ use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
 
 #[ORM\Table(name: 'maps_translation')]
 #[ORM\Index(columns: ['locale'], name: 'maps_locale_idx')]
-#[ORM\Entity(repositoryClass: LocationTranslationRepository::class)]
+#[ORM\Entity(repositoryClass: MapTranslationRepository::class)]
 #[ORM\HasLifecycleCallbacks]
 class MapTranslation implements UuidPrimaryKeyInterface, TranslationInterface, TimestampableInterface
 {
