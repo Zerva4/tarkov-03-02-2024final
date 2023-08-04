f84b18a7c2949a33878424c6068b296f7f83606f
User Tarkov <tarkov@tarkov.town>
2023-08-04 16:25:07 +0000

    Clean main repo

--- src/Interfaces/GraphQLClientInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Interfaces;
-
-interface GraphQLClientInterface
-{
-    public function query(string $query, array $variables = [], ?string $token = null, string $endpoint = null ): string|array;
-}
