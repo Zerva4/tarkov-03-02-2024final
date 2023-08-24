47e55b88b07ecd2840390dc273d7f41458ac686c
Alex Krasnov <alex@i-tools.ru>
2023-08-24 14:23:54 +0500

    Removed legacy js

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-import './js/base.js';
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/front/app.scss';
 
