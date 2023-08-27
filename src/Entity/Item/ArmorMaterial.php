1cdec217feea392ab978f93a97c5910486bce644
Alex Krasnov <alex@i-tools.ru>
2023-08-27 19:52:28 +0500

    Entity for armor material

---
+++ src/Entity/Item/ArmorMaterial.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Item;
+
+use App\Interfaces\Item\ArmorMaterialInterface;
+use App\Interfaces\UuidPrimaryKeyInterface;
+use App\Repository\Item\ArmorMaterialRepository;
+use App\Traits\UuidPrimaryKeyTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+
+#[ORM\Table(name: 'armor_materials', options: ['comment' => 'Таблица для материалов брони'])]
+#[ORM\Entity(repositoryClass: ArmorMaterialRepository::class)]
+class ArmorMaterial implements ArmorMaterialInterface, UuidPrimaryKeyInterface, TimestampableInterface, TranslatableInterface
+{
+    use UuidPrimaryKeyTrait;
+    use TimestampableTrait;
+    use TranslatableTrait;
+
+    #[ORM\Column(type: 'string', length: 32, nullable: false, options: ['default' => '', 'comment' => 'Идентификатор API'])]
+    private string $apiId;
+
+    #[ORM\Column(type: 'float', nullable: false, options: ['default' => 0.0, 'comment' => 'Разрушаемость'])]
+    private float $destructibility;
+
+    #[ORM\Column(type: 'float', nullable: false, options: ['default' => 0.0, 'comment' => 'Мин. деградация при ремонте'])]
+    private float $minRepairDegradation;
+
+    #[ORM\Column(type: 'float', nullable: false, options: ['default' => 0.0, 'comment' => 'Макс. деградация при ремонте'])]
+    private float $maxRepairDegradation;
+
+    #[ORM\Column(type: 'float', nullable: false, options: ['default' => 0.0, 'comment' => 'Разрущаемость от взрыва'])]
+    private float $explosionDestructibility;
+
+    #[ORM\Column(type: 'float', nullable: false, options: ['default' => 0.0, 'comment' => 'Мин. деградация ремкомплектапри ремонте'])]
+    private float $minRepairKitDegradation;
+
+    #[ORM\Column(type: 'float', nullable: false, options: ['default' => 0.0, 'comment' => 'Макс. деградация ремкомплектапри ремонте'])]
+    private float $maxRepairKitDegradation;
+
+    public function getApiId(): string
+    {
+        return $this->apiId;
+    }
+
+    public function setApiId(string $apiId): ArmorMaterialInterface
+    {
+        $this->apiId = $apiId;
+
+        return $this;
+    }
+
+    public function getDestructibility(): float
+    {
+        return $this->destructibility;
+    }
+
+    public function setDestructibility(float $destructibility): ArmorMaterialInterface
+    {
+        $this->destructibility = $destructibility;
+
+        return $this;
+    }
+
+    public function getMinRepairDegradation(): float
+    {
+        return $this->minRepairDegradation;
+    }
+
+    public function setMinRepairDegradation(float $minRepairDegradation): ArmorMaterialInterface
+    {
+        $this->minRepairDegradation = $minRepairDegradation;
+
+        return $this;
+    }
+
+    public function getMaxRepairDegradation(): float
+    {
+        return $this->maxRepairDegradation;
+    }
+
+    public function setMaxRepairDegradation(float $maxRepairDegradation): ArmorMaterialInterface
+    {
+        $this->maxRepairDegradation = $maxRepairDegradation;
+
+        return $this;
+    }
+
+    public function getExplosionDestructibility(): float
+    {
+        return $this->explosionDestructibility;
+    }
+
+    public function setExplosionDestructibility(float $explosionDestructibility): ArmorMaterialInterface
+    {
+        $this->explosionDestructibility = $explosionDestructibility;
+
+        return $this;
+    }
+
+    public function getMinRepairKitDegradation(): float
+    {
+        return $this->minRepairKitDegradation;
+    }
+
+    public function setMinRepairKitDegradation(float $minRepairKitDegradation): ArmorMaterialInterface
+    {
+        $this->minRepairKitDegradation = $minRepairKitDegradation;
+
+        return $this;
+    }
+
+    public function getMaxRepairKitDegradation(): float
+    {
+        return $this->maxRepairKitDegradation;
+    }
+
+    public function setMaxRepairKitDegradation(float $maxRepairKitDegradation): ArmorMaterialInterface
+    {
+        $this->maxRepairKitDegradation = $maxRepairKitDegradation;
+
+        return $this;
+    }
+}
