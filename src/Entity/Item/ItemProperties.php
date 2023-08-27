b2522e73ebd6542374ddb114ed99be18f4995374
Alex Krasnov <alex@i-tools.ru>
2023-08-27 18:56:31 +0500

    Base class for items properties

---
+++ src/Entity/Item/ItemProperties.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity\Item;
+
+use App\Interfaces\Item\ItemInterface;
+use App\Interfaces\Item\ItemPropertiesInterface;
+use App\Interfaces\UuidPrimaryKeyInterface;
+use App\Repository\Item\ItemPropertiesRepository;
+use App\Traits\UuidPrimaryKeyTrait;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: 'items_properties')]
+#[ORM\InheritanceType('JOINED')]
+#[ORM\DiscriminatorColumn(name: 'type_property', type: 'string', length: 50)]
+#[ORM\DiscriminatorMap([
+    'ItemPropertiesAmmo' => ItemPropertiesAmmo::class,
+])]
+#[ORM\Entity(repositoryClass: ItemPropertiesRepository::class)]
+class ItemProperties implements ItemPropertiesInterface, UuidPrimaryKeyInterface
+{
+    use UuidPrimaryKeyTrait;
+
+    #[ORM\OneToOne(inversedBy: 'properties', targetEntity: Item::class)]
+    #[ORM\JoinColumn(name: 'item_id', referencedColumnName: 'id')]
+    private ItemInterface $item;
+
+    public function getItem(): ItemInterface
+    {
+        return $this->item;
+    }
+
+    public function setItem(ItemInterface $item): ItemPropertiesInterface
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+}
