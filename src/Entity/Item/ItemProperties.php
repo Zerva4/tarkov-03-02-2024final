d849cd2ca50c642870ea1c82aecb362c5932d540
Alex Krasnov <alex@i-tools.ru>
2023-08-28 11:20:08 +0500

    Update mapping to armor material mapping

--- src/Entity/Item/ItemProperties.php
+++ src/Entity/Item/ItemProperties.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity\Item;
 
+use App\Interfaces\Item\ArmorMaterialInterface;
 use App\Interfaces\Item\ItemInterface;
 use App\Interfaces\Item\ItemPropertiesInterface;
 use App\Interfaces\UuidPrimaryKeyInterface;
@@ -14,15 +15,20 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\DiscriminatorColumn(name: 'type_property', type: 'string', length: 50)]
 #[ORM\DiscriminatorMap([
     'ItemPropertiesAmmo' => ItemPropertiesAmmo::class,
+    'ItemPropertiesArmor' => ItemPropertiesArmor::class,
 ])]
 #[ORM\Entity(repositoryClass: ItemPropertiesRepository::class)]
 class ItemProperties implements ItemPropertiesInterface, UuidPrimaryKeyInterface
 {
     use UuidPrimaryKeyTrait;
 
-    #[ORM\OneToOne(inversedBy: 'properties', targetEntity: Item::class)]
-    #[ORM\JoinColumn(name: 'item_id', referencedColumnName: 'id')]
+    #[ORM\OneToOne(mappedBy: 'properties', targetEntity: Item::class)]
+    #[ORM\JoinColumn(name: 'item_id', referencedColumnName: 'id', onDelete: 'SET NULL')]
     private ItemInterface $item;
+
+    #[ORM\ManyToOne(targetEntity: ArmorMaterial::class, fetch: 'EAGER', inversedBy: 'properties')]
+    #[ORM\JoinColumn(referencedColumnName: 'id', onDelete: 'cascade')]
+    private ?ArmorMaterialInterface $material = null;
 
     public function getItem(): ItemInterface
     {
@@ -32,6 +38,18 @@ class ItemProperties implements ItemPropertiesInterface, UuidPrimaryKeyInterface
     public function setItem(ItemInterface $item): ItemPropertiesInterface
     {
         $this->item = $item;
+
+        return $this;
+    }
+
+    public function getMaterial(): ?ArmorMaterialInterface
+    {
+        return $this->material;
+    }
+
+    public function setMaterial(?ArmorMaterialInterface $material): ItemPropertiesInterface
+    {
+        $this->material = $material;
 
         return $this;
     }
