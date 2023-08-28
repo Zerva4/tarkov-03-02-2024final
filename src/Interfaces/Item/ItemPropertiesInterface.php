09be94940d9f9dc896147d87009bcdd00ea2c8d5
Alex Krasnov <alex@i-tools.ru>
2023-08-28 17:21:49 +0500

    Update methods

--- src/Interfaces/Item/ItemPropertiesInterface.php
+++ src/Interfaces/Item/ItemPropertiesInterface.php
@@ -10,4 +10,6 @@ interface ItemPropertiesInterface
     public function setItem(ItemInterface $item): ItemPropertiesInterface;
     public function getMaterial(): ?ArmorMaterialInterface;
     public function setMaterial(?ArmorMaterialInterface $material): ItemPropertiesInterface;
+    public function getGrids(): ?ItemStorageGridInterface;
+    public function setGrids(?ItemStorageGridInterface $grids): ItemPropertiesInterface;
 }
