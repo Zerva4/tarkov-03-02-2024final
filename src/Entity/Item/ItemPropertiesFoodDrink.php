5cdce672e692547073e766a6a26cf4e44718fc5f
Alex Krasnov <alex@i-tools.ru>
2023-08-29 10:51:43 +0500

    Entity for food and drink properties

---
+++ src/Entity/Item/ItemPropertiesFoodDrink.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Item;
+
+use App\Interfaces\Item\ItemPropertiesFoodDrinkInterface;
+use App\Interfaces\Item\ItemPropertiesInterface;
+use App\Repository\Item\ItemPropertiesFoodDrinkRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: 'items_properties_food_drink', options: ['comment' => 'Таблица свойств для еды и напитков'])]
+#[ORM\Entity(repositoryClass: ItemPropertiesFoodDrinkRepository::class)]
+class ItemPropertiesFoodDrink extends ItemProperties implements ItemPropertiesInterface, ItemPropertiesFoodDrinkInterface
+{
+    #[ORM\Column(type: 'integer', nullable: false, options: ['default' => 0, 'comment' => ''])]
+    private int $energy;
+
+    #[ORM\Column(type: 'integer', nullable: false, options: ['default' => 0, 'comment' => ''])]
+    private int $hydration;
+
+    #[ORM\Column(type: 'integer', nullable: false, options: ['default' => 0, 'comment' => ''])]
+    private int $units;
+
+    public function getEnergy(): int
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(int $energy): ItemPropertiesFoodDrinkInterface
+    {
+        $this->energy = $energy;
+
+        return $this;
+    }
+
+    public function getHydration(): int
+    {
+        return $this->hydration;
+    }
+
+    public function setHydration(int $hydration): ItemPropertiesFoodDrinkInterface
+    {
+        $this->hydration = $hydration;
+
+        return $this;
+    }
+
+    public function getUnits(): int
+    {
+        return $this->units;
+    }
+
+    public function setUnits(int $units): ItemPropertiesFoodDrinkInterface
+    {
+        $this->units = $units;
+
+        return $this;
+    }
+}
