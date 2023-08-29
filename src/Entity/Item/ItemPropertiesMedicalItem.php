c538c37371beae05a847ee0f97ebca0408af6295
Alex Krasnov <alex@i-tools.ru>
2023-08-29 16:40:09 +0500

    Update method

--- src/Entity/Item/ItemPropertiesMedicalItem.php
+++ src/Entity/Item/ItemPropertiesMedicalItem.php
@@ -3,42 +3,26 @@
 namespace App\Entity\Item;
 
 use App\Interfaces\Item\ItemPropertiesInterface;
-use App\Interfaces\Item\ItemPropertiesKeyInterface;
 use App\Interfaces\Item\ItemPropertiesMedicalItemInterface;
 use App\Repository\Item\ItemPropertiesMedicalItemRepository;
-use App\Traits\TranslatableMagicMethodsTrait;
 use Doctrine\ORM\Mapping as ORM;
-use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
-use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
 
 #[ORM\Table(name: 'items_properties_medical_item', options: ['comment' => 'Таблица свойств для мед. предметов'])]
 #[ORM\Entity(repositoryClass: ItemPropertiesMedicalItemRepository::class)]
-class ItemPropertiesMedicalItem extends ItemProperties implements ItemPropertiesInterface, ItemPropertiesMedicalItemInterface, TranslatableInterface
+class ItemPropertiesMedicalItem extends ItemProperties implements ItemPropertiesInterface, ItemPropertiesMedicalItemInterface
 {
-    use TranslatableTrait;
-    use TranslatableMagicMethodsTrait;
-
     #[ORM\Column(type: 'integer', nullable: false, options: ['default' => 0, 'comment' => 'Используется кол-во раз'])]
     private int $uses;
 
     #[ORM\Column(type: 'integer', nullable: false, options: ['default' => 0, 'comment' => 'Время использования'])]
     private int $useTime;
 
+    #[ORM\Column(type: 'json', nullable: true, options: ["jsonb" => true, 'comment' => ''])]
+    private ?array $cures = null;
+
     public function __construct(string $defaultLocale = '%app.default_locale%')
     {
         $this->defaultLocale = $defaultLocale;
-    }
-
-    public function getCures(): ?array
-    {
-        return $this->translate()->getCures();
-    }
-
-    public function setCures(?array $cures): ItemPropertiesMedicalItemInterface
-    {
-        $this->translate()->setCures($cures);
-
-        return $this;
     }
 
     public function getUses(): int
@@ -61,6 +45,18 @@ class ItemPropertiesMedicalItem extends ItemProperties implements ItemProperties
     public function setUseTime(int $useTime): ItemPropertiesMedicalItemInterface
     {
         $this->useTime = $useTime;
+
+        return $this;
+    }
+
+    public function getCures(): ?array
+    {
+        return $this->cures;
+    }
+
+    public function setCures(?array $cures): self
+    {
+        $this->cures = $cures;
 
         return $this;
     }
