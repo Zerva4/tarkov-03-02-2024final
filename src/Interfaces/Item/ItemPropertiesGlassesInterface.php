691262d4657155cf7de3cbfac204a27368f935d1
Alex Krasnov <alex@i-tools.ru>
2023-08-29 12:46:43 +0500

    Interface for glasses properties entity

---
+++ src/Interfaces/Item/ItemPropertiesGlassesInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Interfaces\Item;
+
+interface ItemPropertiesGlassesInterface
+{
+    public function getClass(): int;
+    public function setClass(int $class): ItemPropertiesGlassesInterface;
+    public function getDurability(): int;
+    public function setDurability(int $durability): ItemPropertiesGlassesInterface;
+    public function getRepairCost(): int;
+    public function setRepairCost(int $repairCost): ItemPropertiesGlassesInterface;
+    public function getBlindnessProtection(): float;
+    public function setBlindnessProtection(float $blindnessProtection): ItemPropertiesGlassesInterface;
+}
