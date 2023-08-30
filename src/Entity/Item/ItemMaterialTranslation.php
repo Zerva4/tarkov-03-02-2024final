bc6bb8e8434275b85c1491ef6a80452749bd0534
Alex Krasnov <alex@i-tools.ru>
2023-08-30 14:07:47 +0500

    Change title to name

---
+++ src/Entity/Item/ItemMaterialTranslation.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Item;
+
+use App\Interfaces\UuidPrimaryKeyInterface;
+use App\Repository\Item\ItemMaterialTranslationRepository;
+use App\Traits\UuidPrimaryKeyTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+
+#[ORM\Table(name: 'items_materials_translation')]
+#[ORM\Index(columns: ['locale'], name: 'item_material_locale_idx')]
+#[ORM\Entity(repositoryClass: ItemMaterialTranslationRepository::class)]
+class ItemMaterialTranslation implements UuidPrimaryKeyInterface, TranslationInterface, TimestampableInterface
+{
+    use UuidPrimaryKeyTrait;
+    use TranslationTrait;
+    use TimestampableTrait;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
