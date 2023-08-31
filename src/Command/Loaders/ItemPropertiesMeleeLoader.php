743b495bdc3a8331712ca52573497c7dc850dfe6
Alex Krasnov <alex@i-tools.ru>
2023-09-01 04:20:56 +0500

    Loader for properties melee

---
+++ src/Command/Loaders/ItemPropertiesMeleeLoader.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Command\Loaders;
+
+use App\Interfaces\Item\ItemPropertiesMeleeInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ItemPropertiesMeleeLoader
+{
+    public function load(
+        EntityManagerInterface $em,
+        ItemPropertiesMeleeInterface $entityProperties,
+        array $arrayProperties,
+        string $locale = '%app.default_locale%'
+    ): ItemPropertiesMeleeInterface
+    {
+        $entityProperties
+            ->setSlashDamage($arrayProperties['slashDamage'] ?? 0)
+            ->setStabDamage($arrayProperties['stabDamage'] ?? 0)
+            ->setHitRadius($arrayProperties['hitRadius'] ?? 0)
+        ;
+
+        return $entityProperties;
+    }
+}
