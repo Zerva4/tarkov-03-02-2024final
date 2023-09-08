6cf2ed3d8bd54bd7030f8ee8585d7785ab615a27
Alex Krasnov <alex@i-tools.ru>
2023-09-08 09:30:08 +0500

    Field for barrel properties type

---
+++ src/Form/Field/ItemPropertiesBarrelField.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form\Field;
+
+use App\Form\Type\ItemPropertiesBarrelType;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+
+class ItemPropertiesBarrelField implements FieldInterface
+{
+    use FieldTrait;
+
+    public static function new(string $propertyName, ?string $label = null, $fieldsConfig = []): ItemPropertiesBarrelField
+    {
+        return (new self())
+            ->setProperty($propertyName)
+            ->setLabel($label)
+            ->setFormType(ItemPropertiesBarrelType::class)
+            ->setCssClass('field-properties-barrel')
+            ->hideOnIndex()
+            ->hideOnDetail()
+        ;
+    }
+}
