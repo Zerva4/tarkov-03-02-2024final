d6ee757562de542ae10c53945efc371b6996a627
Alex Krasnov <alex@i-tools.ru>
2023-09-19 20:29:39 +0500

    Updated

--- src/Controller/BulletsController.php
+++ src/Controller/BulletsController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\Item\ItemCaliberRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class BulletsController extends AbstractController
 {
     #[Route('/bullets', name: 'app_bullets')]
-    public function index(): Response
+    public function index(ItemCaliberRepository $caliberRepository): Response
     {
         return $this->render('bullets/index.html.twig', [
+            'calibers' => $caliberRepository->findAll()
         ]);
     }
 }
