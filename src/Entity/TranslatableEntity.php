1db32e1ffe951ac24cb48549d9f16111bfc0eede
Alex Krasnov <alex@i-tools.ru>
2023-10-25 20:04:23 +0500

    Updated

--- src/Entity/TranslatableEntity.php
+++ src/Entity/TranslatableEntity.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Traits\TranslatableMagicMethodsTrait;
+use Doctrine\Common\Collections\ArrayCollection;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
 use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
 use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
@@ -14,21 +15,22 @@ abstract class TranslatableEntity implements TimestampableInterface, Translatabl
 {
     use TimestampableTrait;
     use TranslatableTrait;
-//    use TranslatableMagicMethodsTrait;
+    use TranslatableMagicMethodsTrait;
 
     public function __construct(string $defaultLocale = '%app.default_locale%')
     {
         $this->defaultLocale = $defaultLocale;
+        $this->translations = new ArrayCollection();
     }
 
-//    private function proxyCurrentLocaleTranslation(string $method, array $arguments = [])
-//    {
-//        if (!method_exists(self::getTranslationEntityClass(), $method)) {
-//            $method = 'get' . ucfirst($method);
-//        }
-//
-//        $translation = $this->translate($this->getCurrentLocale());
-//
-//        return (method_exists(self::getTranslationEntityClass(), $method)) ? call_user_func_array([$translation, $method], $arguments) : null;
-//    }
+    private function proxyCurrentLocaleTranslation(string $method, array $arguments = [])
+    {
+        if (!method_exists(self::getTranslationEntityClass(), $method)) {
+            $method = 'get' . ucfirst($method);
+        }
+
+        $translation = $this->translate($this->getCurrentLocale());
+
+        return (method_exists(self::getTranslationEntityClass(), $method)) ? call_user_func_array([$translation, $method], $arguments) : null;
+    }
 }
