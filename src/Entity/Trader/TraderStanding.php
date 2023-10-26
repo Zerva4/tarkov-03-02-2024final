5878be26063dec67634d4fd274e5d97aefb13e12
Alex Krasnov <alex@i-tools.ru>
2023-10-26 19:42:06 +0500

    Added field apiId

--- src/Entity/Trader/TraderStanding.php
+++ src/Entity/Trader/TraderStanding.php
@@ -14,10 +14,14 @@ use App\Traits\UuidPrimaryKeyTrait;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Table(name: 'traders_standings')]
+#[ORM\Index(columns: ['api_id'], name: 'standings_api_key_idx')]
 #[ORM\Entity(repositoryClass: TraderStandingRepository::class)]
 class TraderStanding implements TraderStandingInterface, UuidPrimaryKeyInterface
 {
     use UuidPrimaryKeyTrait;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private string $apiId;
 
     #[ORM\ManyToOne(targetEntity: Quest::class, inversedBy: 'neededKeys')]
     #[ORM\JoinColumn(onDelete: 'CASCADE')]
@@ -29,6 +33,18 @@ class TraderStanding implements TraderStandingInterface, UuidPrimaryKeyInterface
 
     #[ORM\Column(type: 'float', nullable: false, options: ['default' => 0 ])]
     private float $standing;
+
+    public function getApiId(): string
+    {
+        return $this->apiId;
+    }
+
+    public function setApiId(string $apiId): TraderStandingInterface
+    {
+        $this->apiId = $apiId;
+
+        return $this;
+    }
 
     public function getTrader(): TraderInterface
     {
