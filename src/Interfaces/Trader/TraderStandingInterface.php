d90a0f0c1504dbd6efcb27f149a9e396adbbc9b7
Alex Krasnov <alex@i-tools.ru>
2023-10-26 20:06:19 +0500

    Update methods

--- src/Interfaces/Trader/TraderStandingInterface.php
+++ src/Interfaces/Trader/TraderStandingInterface.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Interfaces\Trader;
 
+use App\Interfaces\Quest\QuestInterface;
+
 interface TraderStandingInterface
 {
     public function getApiId(): string;
     public function setApiId(string $apiId): TraderStandingInterface;
+    public function getQuest(): ?QuestInterface;
+    public function setQuest(?QuestInterface $quest): TraderStandingInterface;
     public function getTrader(): TraderInterface;
     public function setTrader(TraderInterface $trader): TraderStandingInterface;
     public function getStanding(): float;
