4b80e8b18dd1b07a3bd146e02b8b93753320035e
Alex Krasnov <alex@i-tools.ru>
2023-11-30 19:13:44 +0500

    Base controller for article

--- src/Controller/MechanicsController.php
+++ src/Controller/MechanicsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class MechanicsController extends AbstractController
 {
     #[Route('/mechanics', name: 'app_mechanics')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em, PaginatorInterface $paginator, Request $request): Response
     {
+        $dql   = "SELECT a FROM App\Entity\Article\Article a";
+        $query = $em->createQuery($dql);
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
         return $this->render('mechanics/index.html.twig', [
-            'controller_name' => 'MechanicsController',
+            'pagination' => $pagination,
         ]);
     }
 }
