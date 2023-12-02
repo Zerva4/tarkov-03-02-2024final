5a14ed10d3c1cd6a7233c657c49d837b7ddce582
Alex Krasnov <alex@i-tools.ru>
2023-12-03 01:27:39 +0500

    Loader for stimulation properties

---
+++ src/Command/Loaders/ItemPropertiesStimulationLoader.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Command\Loaders;
+
+use App\Entity\Item\StimulationEffect;
+use App\Interfaces\Item\Properties\ItemPropertiesStimulationInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ItemPropertiesStimulationLoader
+{
+    public function load(
+        EntityManagerInterface $em,
+        ItemPropertiesStimulationInterface $entityProperties,
+        array $arrayProperties,
+        string $locale = '%app.default_locale%'
+    ): ItemPropertiesStimulationInterface
+    {
+        $stimulationEffectEntity = new StimulationEffect($locale);
+        $stimulationEffectEntity
+            ->setType($arrayProperties['stimEffects']['useTime'] ?? null)
+            ->setChance($arrayProperties['stimEffects']['chance'] ?? null)
+            ->setDelay($arrayProperties['stimEffects']['delay'] ?? null)
+            ->setDuration($arrayProperties['stimEffects']['duration'] ?? 0)
+            ->setValue($arrayProperties['stimEffects']['value'] ?? null)
+            ->setPercent($arrayProperties['stimEffects']['percent'] ?? false)
+            ->setSkillName($arrayProperties['stimEffects']['skillName'] ?? 0)
+        ;
+
+        $entityProperties
+            ->setUseTime($arrayProperties['useTime'] ?? 0)
+            ->setCures($arrayProperties['cures'] ?? [])
+            ->setStimulationEffect($stimulationEffectEntity)
+        ;
+        return $entityProperties;
+    }
+}
