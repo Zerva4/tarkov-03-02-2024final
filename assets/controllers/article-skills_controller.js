f8d534076a9abfe757813c5ed8966ab4b5196c01
Zerva4 <4zerva@gmail.com>
2023-12-27 02:04:44 +0500

    fix+bullets

---
+++ assets/controllers/article-skills_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    tabs = document.querySelectorAll('.select_abils');
+
+    initialize()
+    {
+        super.initialize();
+    }
+
+    connect()
+    {
+        this.tabs.forEach((item) => {
+            item.addEventListener('click', (e) => this.switchSkills(e));
+        });
+    }
+
+    switchSkills(event)
+    {
+        let tabs_parent = event.target.closest('.abils');
+        let active_switch = tabs_parent.querySelector('.select_abils.active');
+        let active_tab = tabs_parent.querySelector('.abil.active');
+        let target_tab = tabs_parent.querySelector('#' + this.getAttribute('data-abil'));
+
+        if (active_switch) {
+            active_switch.classList.remove('active');
+        }
+
+        if (active_tab) {
+            active_tab.classList.remove('active');
+        }
+
+        if (target_tab) {
+            target_tab.classList.add('active');
+            event.target.classList.add('active');
+        }
+    }
+}
