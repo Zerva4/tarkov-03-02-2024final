390ce0a0f7e90a1e4d5d69ac66561d71de69a0ca
Zerva4 <4zerva@gmail.com>
2024-02-02 03:00:48 +0500

    last-news test

--- src/Repository/Article/ArticleRepository.php
+++ src/Repository/Article/ArticleRepository.php
@@ -59,7 +59,7 @@ class ArticleRepository extends ServiceEntityRepository
                 'type' => $type,
                 'locale' => $locale,
             ])
-            ->setFirstResult($offset)
+            ->setFirstResult(0)
             ->setMaxResults($maxItem)
             ->getQuery()
             ->setResultCacheLifetime(0)
