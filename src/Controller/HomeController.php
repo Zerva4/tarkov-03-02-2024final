59c72bdfbacabcddb4e5c8aff7b1d779de399173
Zerva4 <4zerva@gmail.com>
2024-02-02 05:49:08 +0500

    last-news test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends AbstractController
             'traders' => $tradersList,
             'lastArticles' => $articlesList,
             'newsCategories' => $newsCategories,
-            '$newsMain' => $NewsList
+            'newsMain' => $NewsList
         ]);
     }
 }
